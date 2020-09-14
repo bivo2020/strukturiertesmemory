7621b98216072abc2f786615b7831e784b6c4d8c
Dr. Volker Mühlenkamp <dr.muehlenkamp@givo-ifbb.de>
2020-09-14 22:42:40 +0200

    Umgestaltung

--- memory.js
+++ memory.js
@@ -1,4 +1,5 @@
-var bildeins=-1, bildzwei=-1;
+var bildeins = -1,
+  bildzwei = -1;
 
 var deckblatt = "bilder/DeckBlatt.gif";
 var bilder = [];
@@ -6,39 +7,29 @@ bilder[0] = "bilder/1.gif";
 bilder[1] = "bilder/2.gif";
 bilder[2] = "bilder/3.gif";
 bilder[3] = "bilder/4.gif";
-bilder[4] = "bilder/1.gif";
-bilder[5] = "bilder/2.gif";
-bilder[6] = "bilder/3.gif";
-bilder[7] = "bilder/4.gif";
+bilder = bilder.concat(bilder);
 
 function klickAufBild(that, bildnr) {
+  if (bildeins == bildnr || bildzwei == bildnr) return;
   if (bildeins == -1) {
     that.src = bilder[bildnr];
     bildeins = bildnr;
-  }
-  else if (bildzwei == -1) {
+  } else if (bildzwei == -1) {
     that.src = bilder[bildnr];
     bildzwei = bildnr;
-  }
-  else {
+  } else {
     let memoryspielfeld = document.getElementById("memoryspielfeld");
     if (bilder[bildeins] == bilder[bildzwei]) {
       memoryspielfeld.getElementsByTagName("img")[bildeins].src = "";
       memoryspielfeld.getElementsByTagName("img")[bildzwei].src = "";
-      bildeins = -1;
-      bildzwei = -1;
-      that.src = bilder[bildnr];
-      bildeins = bildnr;
-    }
-    else {
+      memoryspielfeld.getElementsByTagName("img")[bildeins].onclick = "";
+      memoryspielfeld.getElementsByTagName("img")[bildzwei].onclick = "";
+    } else {
       memoryspielfeld.getElementsByTagName("img")[bildeins].src = deckblatt;
       memoryspielfeld.getElementsByTagName("img")[bildzwei].src = deckblatt;
-      bildeins = -1;
-      bildzwei = -1;
-      that.src = bilder[bildnr];
-      bildeins = bildnr;
     }
-
+    bildzwei = -1;
+    that.src = bilder[bildnr];
+    bildeins = bildnr;
   }
-
 }
